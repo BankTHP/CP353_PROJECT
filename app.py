9b4e61649e8ecda92c2524fd9aa1ef2c502fda47
BankTHP <bang.225588z@gmail.com>
2021-05-15 17:55:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ app.register_blueprint(index)
 
 
 if __name__ == '__main__':
-    app.run(host='localhost',port=5000,debug=True,use_reloader=True)
+    app.run(host='localhost',port=5050,debug=True,use_reloader=True)
