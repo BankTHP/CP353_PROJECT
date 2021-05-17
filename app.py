65aa2ad41cf98c6a2213fe7edd6da142ca9be78d
BankTHP <bang.225588z@gmail.com>
2021-05-17 18:06:16 +0700

    wo api

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template,request
+from werkzeug import datastructures
 from flask_cors import CORS
 from flask_jwt_extended.jwt_manager import JWTManager
 from flask_restful import Api
 from PIL import Image
 import base64
 from ml import TFModel
-import os,json
+import os,json,requests
 from flask_sqlalchemy import SQLAlchemy
 from api.routes import create_route
 from flask_swagger_ui import get_swaggerui_blueprint
@@ -50,13 +51,17 @@ app.register_blueprint(SWAGGER_UI_BLUEPRINT, url_prefix=SWAGGER_URL)
 # init JWT
 jwt = JWTManager(app=app)
 
+
+
 @app.route("/")
 def home():
-    data = json.load(open('cat.json'))
+    data = readJson()
     return render_template("index.html",data = data)
+
 
 @app.route('/catscan', methods=['GET', 'POST'])
 def upload_file():
+    data = readJson()
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'there is no file in form!'
@@ -66,14 +71,23 @@ def upload_file():
 
         imagecat = Image.open(path)
         outputs = model.predict(imagecat)
-        data = readJson()
+        imagename = imagecat.filename
         cats = []
         name = outputs['predictions'][0]['label']
         for i in data:
             if  name == i['Name']:
-                cats.append(i)
-            
-        imagename = imagecat.filename()
+                cats.append(i)   
+        cats.append({
+                "Name": "ไม่มีข้อมูล",
+                "LifeSpanMin": "ไม่มีข้อมูล",
+                "LifeSpanMax": "ไม่มีข้อมูล",
+                "Color": "ไม่มีข้อมูล",
+                "Coat": "ไม่มีข้อมูล",
+                "Energy": "ไม่มีข้อมูล",
+                "Shedding": "ไม่มีข้อมูล",
+                "Size": "ไม่มีข้อมูล"
+        },)
+                
         return render_template('prediction.html', pred_result=outputs,pic = imagename,cats = cats)
 
     return render_template('upload.html')
@@ -83,3 +97,4 @@ def readJson():
     read_json_file = open_json_file.read()
     cat_data = json.loads(read_json_file)
     return cat_data
+
--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-import json
-def readJson():
-    open_json_file = open('faculty.json', 'r',encoding="utf8") 
-    read_json_file = open_json_file.read()
-    cat_data = json.loads(read_json_file)
-    return cat_data
-test = readJson()
-listname = []
-for i in range(len(test)) :
-    listname.append(str(test[i]['lon'])+",")
-for i in range(len(listname)) :
-    print(listname[i])
+import json,requests,jsonify
+url = "http://127.0.0.1:5000/cat"
+data= requests.request("GET", url).json()
+print(jsonify(url))
