ffb392361987728595f70b3bac0ca82fe30bd011
BankTHP <bang.225588z@gmail.com>
2021-05-19 08:11:30 +0700

    final

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-data = json.load(open('faculty.json',encoding="utf8"))
-listcheck = []
-for i in range(len(data)):
-    listcheck.append(data[i]['facultyName'])
-for i in range(len(listcheck)):
-    print('"'+listcheck[i]+'",')
-
